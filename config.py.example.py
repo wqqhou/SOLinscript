1ec7e1d5e0af2a2a64c3b1a9082c392609403492
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2023-11-24 23:32:54 +0800

    Update config.py.example.py

--- config.py.example.py
+++ config.py.example.py
@@ -1,6 +1,6 @@
-RPC = 'https://solemn-cosmological-tree.solana-mainnet.quiknode.pro/18afa712ac587ae0f891514d0b99582d59304a0b/'
+RPC = ''
 
-private_key = '21iR7hrER6QsTmPuvsZEENhGyUhhNJZriEypxCFHH46uPvmTqnc5B7rnkhZgvfgANLf1gNSt1292beTfr3kw2XV2'
+private_key = ''
 
 inscription = '{"p":"src-20","op":"mint","tick":"sols","amt":"1000"}'
 
